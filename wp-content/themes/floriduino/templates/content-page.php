beac83a58bdcab876439dc2a92702cd0f6dbde93
Richard Stovall <rstovall@me.com>
2017-03-30 11:44:00 -0400

    Adding .gitignore

---
+++ wp-content/themes/floriduino/templates/content-page.php
@@ -0,0 +1,26 @@
+<div class="wrap container" role="document">
+  <div class="content row">
+  	<div class="page-header">
+  	  <h1><?php echo get_field('title'); ?></h1>
+  	  <h3><?php echo get_field('subtitle'); ?></h3>
+  	  <hr>
+  	</div>
+  </div><!-- /.content -->
+</div><!-- /.wrap -->
+
+<div class="wrap container" role="document">
+  <div class="content row">
+    <?php the_content(); ?>
+  </div><!-- /.content -->
+</div><!-- /.wrap -->
+
+<br><br>
+<!--
+<div class="background light-blue">
+	<div class="wrap container" role="document">
+	  <div class="content row">
+	    <iframe src="https://docs.google.com/a/codeydevelopment.com/forms/d/e/1FAIpQLSeGZy706vxBTQxNez6UaHz6mlg0PzSFw3_MDVFABEfRK-98vg/viewform?embedded=true" width="760" height="1000" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+	  </div>
+	</div>
+</div>
+-->
---
+++ wp-content/themes/floriduino/templates/header.php
@@ -0,0 +1,7 @@
+<header class="banner">
+  <div class="container">
+	<a href="<?php echo esc_url( home_url()); ?>">
+    <img src="/wp-content/themes/floriduino/dist/images/logo-arduino-day-expanded.svg" alt="arduino day logo">
+	</a>
+  </div>
+</header>
